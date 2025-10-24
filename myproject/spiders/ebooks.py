acd8016510af4be4152eecca9cbe9278ac9a26fe
asadibrahim206 <asad.denin@gmail.com>
2025-10-24 23:11:00 +0500

    nothin special

--- myproject/spiders/ebooks.py
+++ myproject/spiders/ebooks.py
@@ -5,4 +5,5 @@ class EbookSpider(scrapy.Spider):
     name = "ebook"
     starts_url = ["https://books.toscrape.com/"]
     def parse(self,response):
+        print("SCRAPYYYYYYYYYYYYYYYYY")
         print(response)
