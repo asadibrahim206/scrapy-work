34972b9729518cdf2c0febedff04ae45d8d6a6ad
asadibrahim206 <asad.denin@gmail.com>
2025-10-24 22:27:41 +0500

    nothin special

--- myproject/spiders/ebooks.py
+++ myproject/spiders/ebooks.py
@@ -1,5 +1,8 @@
 import scrapy
-from scrapy import spider
+from scrapy import Spider
 
 class EbookSpider(scrapy.Spider):
     name = "ebook"
+    starts_url = ["https://books.toscrape.com/"]
+    def parse(self,response):
+        print(response)
