ba3a3308f491686aae3da43252fb4f2e029146ac
asadibrahim206 <asad.denin@gmail.com>
2025-10-25 15:53:42 +0500

    creating spider class

--- myproject/spiders/ebook-2.py
+++ myproject/spiders/ebook-2.py
@@ -4,3 +4,19 @@ class EbookSpider(scrapy.Spider):
     name = "ebooks"
 
     start_urls = ["https://books.toscrape.com/"]
+
+    def parse(self,response):
+        print("[PARSE]")
+        articles = response.css("article")
+        for article in articles:
+            item = article.css("h3 a::text").get()
+            price = article.css(".price_color::text").get()
+            print(item,price)
+            yield{
+                "item", item,
+                "price", price
+            }
+
+
+
+    
