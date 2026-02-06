56b70cc8903d92d77892834357eb604641c4246e
asadibrahim206 <asad.denin@gmail.com>
2026-02-06 12:17:11 +0500

    new

--- scrape/ebook_scraper/ebook_scraper/spiders/ebook.py
+++ scrape/ebook_scraper/ebook_scraper/spiders/ebook.py
@@ -1,1 +1,18 @@
 import scrapy
+
+class EbookSpider(scrapy.Spider):
+    name = "ebook"
+    start_urls =["https://books.toscrape.com/"]
+
+    def parse(self,response):
+        print("[----------OUR RESPONSE----------]")
+        ebooks = response.css("article.product_pod")
+
+        for ebook in ebooks:
+            # title = ebook.css("h3 a::attr(title)").get()
+              title = ebook.css("h3 a").attrib['title']
+              price = ebook.css("p.price_color::text").get()
+
+              yield{
+                   "title":title, "price":price
+              }   
