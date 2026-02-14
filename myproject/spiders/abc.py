ae849b6a1b8e393b5010ea385c6733e838ffb2d4
asadibrahim206 <asad.denin@gmail.com>
2026-02-14 19:57:43 +0500

    n

--- myproject/spiders/abc.py
+++ myproject/spiders/abc.py
@@ -1,4 +1,9 @@
-import scrapy
+from scrapy import Spider
 
-class TrackerSpider:
+class TrackerSpider(Spider):
     name = 'tracker'
+    start_urls = ["https://www.redfin.com/city/9361/CA/Irvine"]
+
+    def parse(self,response):
+        print("our parse")
+        print(response.body)
