2aff52dabb1833a6e5990147a6a70cf14fd63d19
asadibrahim206 <asad.denin@gmail.com>
2026-02-22 20:07:16 +0500

    new spider

--- myproject/spiders/outfitter.py
+++ myproject/spiders/outfitter.py
@@ -1,13 +1,13 @@
 import scrapy
 
 class OutfitterSpider(scrapy.Spider):
-    name = "outfitter"
-    start_urls = ["https://outfitters.com.pk/collections/men-t-shirts"]
+    name = "ebook"
+    start_urls = ["https://books.toscrape.com/"]
 
     def parse(self, response):
         print("[PARSE]")
-        items = response.xpath("//h3[@class='card__heading h5']/a")
-        prices = response.xpath("//span[@class='money']/text()")
+        items = response.css("//h3[@class='card__heading h5']/a")
+        prices = response.("//span[@class='money']/text()")
         
         for item in items:
             item = item.xpath("//h3[@class='card__heading h5']/a/text()")
