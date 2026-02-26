750c8052c03495530e38d5166b3aa7fb5acab401
asadibrahim206 <asad.denin@gmail.com>
2026-02-26 20:42:46 +0500

    scraped item

--- myproject/spiders/outfitter.py
+++ myproject/spiders/outfitter.py
@@ -12,10 +12,10 @@ class OutfitterSpider(scrapy.Spider):
         for product in details:
             item = ProductItem()
             
+
             raw_title = product.css("h4.p-title::text").get()
             
-            # Use ::text to get the actual price number
-            # Using .re_first helps pull only the digits and commas
+          
             raw_price = product.css(".price-diff-retail span::text").re_first(r'[0-9,]+')
 
             item['title'] = raw_title.strip() if raw_title else None
